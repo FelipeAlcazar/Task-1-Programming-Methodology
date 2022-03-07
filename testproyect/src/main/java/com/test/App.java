b4ef17823b88075bc850c8b65e112133fe7bdd41
ElenaMegia <72968353+ElenaMegia@users.noreply.github.com>
2022-03-07 12:45:44 +0100

    Update App.java

--- testproyect/src/main/java/com/test/App.java
+++ testproyect/src/main/java/com/test/App.java
@@ -4,16 +4,35 @@ import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
+/*********************************************************************
+*
+* Class Name: App
+* Author/s name:
+* Release/Creation date:
+* Class description: In this class we execute de program
+*
+**********************************************************************
+*/
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+ /*********************************************************************
+*
+* Method name: main
+*
+* Description of the Method: In this method we inicializa the program and call the method
+* getTimeFileAndOutput with which we are going to create the excel.
+*
+* Checked Exceptions, we use two exceptions:
+* IOException: When there is an error in input or output.
+* InvalidFormatException:  is used when the underlying problem appears to be that of bad 
+* formatting of a value to deserialize.
+*
+*********************************************************************/
+    
     public static void main( String[] args ) throws IOException, InvalidFormatException
     {
-        int[] sizeN={100,500,1000,5000,8000,9000,10000,11000,20000,50000};
+        int[] sizeN={100,500,1000,5000,8000,9000,10000,11000,20000,50000}; //It is one-dimensional array with the values of the sizes of the arrays of the program.
         
         //1 for nano, any other number for millis
         Array.getTimeFileAndOutput(sizeN,1);
