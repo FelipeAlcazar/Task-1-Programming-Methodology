a831fc433a4180174c0ea6dc9a0f0c662f5b422f
ElenaMegia <72968353+ElenaMegia@users.noreply.github.com>
2022-03-14 19:08:14 +0100

    Update App.java

--- testproyect/src/main/java/com/test/App.java
+++ testproyect/src/main/java/com/test/App.java
@@ -33,8 +33,31 @@ public class App
     public static void main( String[] args ) throws IOException, InvalidFormatException
     {
         int[] sizeN={100,500,1000,5000,8000,9000,10000,11000,20000,50000}; //It is one-dimensional array with the values of the sizes of the arrays of the program.
-        
+        System.out.println("Welcome to the program.");
+        System.out.println("Choose the measure of time you want: 1 for nano, 2 for mili.");
+        int number=chooseTime();
         //1 for nano, any other number for millis
-        Array.getTimeFileAndOutput(sizeN,1);
+        Array.getTimeFileAndOutput(sizeN,number);
     }
+ 
+  /*********************************************************************
+*
+* Method name: chooseTime
+*
+* Description of the Method: In this method we ask if you want the time in nano or mili.
+*
+* Return value: int, number, number=1 -> nano number=2->mili
+*
+*********************************************************************/
+  public static int chooseTime(){
+        int number=0;
+        
+        number=sc.nextInt();
+        while (number != 1 && number != 2) {
+            System.out.print("Introduce a number between 1 and 2");
+            number = sc.nextInt();
+            }
+            return number;
+    }
+ 
 }
